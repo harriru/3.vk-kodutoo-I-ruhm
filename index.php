aa5e5097535a5e9133251671960f84ffe9ee4438
Harri Ruttas <harrrut@greeny.cs.tlu.ee>
2016-05-30 02:11:28 +0300

    loon php file sisu

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
-<?php>
+<!DOCTYPE html >
+<head>
+<title>Chat Room</title>
+
+<link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+ 
+<div id="wrapper">
+    <div id="menu">
+        <p class="welcome">Welcome, <b></b></p>
+        <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+        <div style="clear:both"></div>
+    </div>
+     
+    <div id="chatbox"></div>
+     
+    <form name="message" action="">
+        <input name="usermsg" type="text" id="usermsg" size="63" />
+        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+    </form>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script src="app.js" charset="utf-8"></script>
+<script type="text/javascript">
+
+
+</script>
+</body>
+</html>
